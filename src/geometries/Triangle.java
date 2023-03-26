9dcbf33d697a26621e1f437a046ac6128f562984
etyali <etya@g.jct.ac.il>
2023-03-26 20:07:36 +0300

    base geometries

---
+++ src/geometries/Triangle.java
@@ -0,0 +1,9 @@
+package geometries;
+
+import primitives.Point;
+
+public class Triangle extends Polygon{
+    public Triangle(Point... vertices) throws Exception {
+        super(vertices);
+    }
+}
--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,2 +1,28 @@
-package primitives;public class Ray {
+package primitives;
+
+import java.util.Objects;
+
+public class Ray {
+    public Point p0;
+    public Vector dir;
+
+    public Ray(Point p0, Vector dir) {
+        this.p0 = p0;
+        this.dir = dir.normalize();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ray ray)) return false;
+        return Objects.equals(p0, ray.p0) && Objects.equals(dir, ray.dir);
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "p0=" + p0 +
+                ", dir=" + dir +
+                '}';
+    }
 }
