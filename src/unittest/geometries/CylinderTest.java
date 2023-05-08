61ebe863d6ddcdf00c6929559efa54bc6824eac3
etyali <etya@g.jct.ac.il>
2023-05-08 10:05:40 +0300

    new test find intersection plane

--- src/unittest/geometries/CylinderTest.java
+++ src/unittest/geometries/CylinderTest.java
@@ -1,5 +1,7 @@
 package unittest.geometries;
 
+import java.util.*;
+
 import geometries.Cylinder;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
@@ -12,7 +14,7 @@ class CylinderTest {
 
     @Test
     void testGetNormal() throws Exception {
-        Cylinder c = new Cylinder(new Ray(new Point(0,0,0), new Vector(0,0,1)),2,2);
+        Cylinder c = new Cylinder(new Ray(new Point(0, 0, 0), new Vector(0, 0, 1)), 2, 2);
 
         // ============ Equivalence Partitions Tests ==============
 
@@ -29,6 +31,7 @@ class CylinderTest {
                 "Bad normal for a point on the parallel base of the cylinder where the axis ray starts");
 
         // =============== Boundary Values Tests ==================
+
         //TC01: test for normal to point on the base edge
         assertEquals(c.getNormal(new Point(2, 0, 2)), new Vector(0, 0, 1),
                 "Incorrect normal for a point on the base edge of the cylinder");
@@ -45,4 +48,30 @@ class CylinderTest {
         assertEquals(c.getNormal(new Point(0, 0, 2)), new Vector(0, 0, 1),
                 "Incorrect normal for a center point on the base of the cylinder");
     }
+/*
+    public void testFindIntersaction() {
+        Cylinder c = new Cylinder(new Ray(new Point(1, 0, 0), new Vector(0, 0, 1)), 1, 1);
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: Ray's line is outside the cylinder (zero intersections)
+        assertNull(c.findIntersections(new Ray(new Point(-0.81838, 0.48601, 0), new Vector(1.82, 1.6, 1))),
+                "Ray's line out of cylinder");
+        // TC02: Ray starts inside the cylinder (1 intersection)
+        List<Point> result1 = c.findIntersections(new Ray(new Point(0.81838, 0.48601, 0.56951),
+                new Vector(0.18, 1.6, 0.43)));
+        assertEquals(1, result1.size(), "Wrong number of points");
+        // TC03: Ray starts before and crosses the cylinder (2 intersections)
+        List<Point> result2 = c.findIntersections(new Ray(new Point(0.81838, -0.48601, -0.32453),
+                new Vector(0.18, 2.57, 1.44)));
+        assertEquals(2, result2.size(), "Wrong number of points");
+
+        // TC04: Ray starts after the cylinder (0 intersections)
+        assertNull(c.findIntersections(new Ray(new Point(1, 1, 1.5), new Vector(0, 1.09, 0.5))),
+                "Ray's line out of cylinder");
+
+        // =============== Boundary Values Tests ==================
+        }
+*/
+
+
 }
--- src/unittest/geometries/PlaneTest.java
+++ src/unittest/geometries/PlaneTest.java
@@ -3,8 +3,13 @@ package unittest.geometries;
 import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
+import primitives.Ray;
 import geometries.Plane;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class PlaneTest {
 
@@ -19,6 +24,22 @@ class PlaneTest {
         // TC01: A simple single test here (all the points are the same)
         double sqrt3 = Math.sqrt(1d / 3);
         assertEquals(new Vector(sqrt3, sqrt3, sqrt3), p.getNormal(), "Incorrect normal to plane");
+
+    }
+
+    public void testFindIntersaction() throws Exception {
+        Plane plane = new Plane(new Point(-3, -3, 1), new Point(3, -3, 1),
+                new Point(1, 5, 1));
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: Ray's line intersect the plane (1 intersection)
+        List<Point> result1 = plane.findIntersections(new Ray(new Point(1, 3, 0),
+                new Vector(0, 0, 2)));
+        assertEquals(1, result1.size(), "Wrong number of points")
+
+        // TC01: Ray's line does not intersect the plane (0 intersection)
+        assertNull(plane.findIntersections(new Ray(new Point(-0.81838, 0.48601, 0), new Vector(1.82, 1.6, 1))),
+                "Ray's line out of cylinder");
 
     }
 
